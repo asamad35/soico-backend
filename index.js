e569b181c033a069a843c27f4c8ab849aa1230e0
asamad35 <samad.abdus3535@gmail.com>
2023-02-22 21:32:29 +0530

    updates

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    // origin: "https://socio-plus.netlify.app",
+    // origin: "http://localhost:5173",
+    origin: "https://socio-plus.netlify.app",
     methods: ["GET", "POST"],
   },
 });

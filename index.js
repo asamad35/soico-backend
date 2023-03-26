a080b4f5f418559c15422aec0c390afc2ee18665
asamad35 <samad.abdus3535@gmail.com>
2023-03-26 18:11:23 +0530

    vercel test1

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const io = new Server(server, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
 let onlineUsers = [];
 
